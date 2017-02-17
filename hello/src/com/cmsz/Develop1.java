cdbe0ecc6fc12adbac89e0063aa12984bc52a416
chenxy <1063975796@qq.com>
2017-02-17 13:46:07 +0800

    develop2

--- hello/src/com/cmsz/Develop1.java
+++ hello/src/com/cmsz/Develop1.java
@@ -4,6 +4,6 @@ public class Develop1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Develop1");
-		System.out.println("develop 2");
+		System.out.println("develop 222222");
 	}
 }
